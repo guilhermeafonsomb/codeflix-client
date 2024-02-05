f34c3f5f97710d7123a0b6d2b566299571e171ce
Guilherme Afonso <guilhermeafonsomb@gmail.com>
2024-02-05 16:19:12 -0300

    doing header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,26 @@
+'use client';
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    }
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+
   return (
-    <header className="fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 lg:px-10 lg:py-6 transition-all">
+    <header className={`${isScrolled && 'bg-black'} fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 lg:px-10 lg:py-6 transition-all`}>
       <div className="flex items-center space-x-2 md:space-x-8">
         <Image src="/logo.svg" alt="netflix" width={120} height={120} />
         <ul className="hidden md:flex md:space-x-4">
