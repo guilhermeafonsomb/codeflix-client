10d35adf4afb72be843d44e13a778758eccbc33d
Guilherme Afonso <guilhermeafonsomb@gmail.com>
2024-02-05 16:10:16 -0300

    doing header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 lg:px-10 lg:py-6">
+    <header className="fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 lg:px-10 lg:py-6 transition-all">
       <div className="flex items-center space-x-2 md:space-x-8">
         <Image src="/logo.svg" alt="netflix" width={120} height={120} />
         <ul className="hidden md:flex md:space-x-4">
@@ -12,6 +12,13 @@ export default function Header() {
           <li>Latest</li>
         </ul>
       </div>
+
+      <div className="flex items-center space-x-4 md:space-x-8">
+        <p className="hidden cursor-not-allowed lg:inline">Kids</p>
+        <Image src="/profile.png" alt="profile" width={30} height={30} className="cursor-pointer rounded" />
+
+      </div>
+
     </header>
   );
 }
